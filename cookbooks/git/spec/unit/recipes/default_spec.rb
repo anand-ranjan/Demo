5a78a13917aa6dab03dd56996e3d5965f4bde790
Anand Ranjan <ar.anand.ranjan@gmail.com>
2017-12-06 03:34:06 +1100

    Added a Unit Test case using chefspec.

--- cookbooks/git/spec/unit/recipes/default_spec.rb
+++ cookbooks/git/spec/unit/recipes/default_spec.rb
@@ -20,29 +20,16 @@
 require 'spec_helper'
 
 describe 'git::default' do
-  context 'When all attributes are default, on Ubuntu 16.04' do
+  context 'package install, on Ubuntu 12.04' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '12.04')
       runner.converge(described_recipe)
     end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
-    end
-  end
-
-  context 'When all attributes are default, on CentOS 7.4.1708' do
-    let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708')
-      runner.converge(described_recipe)
-    end
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'installed git' do
+      expect(chef_run).to install_package('git')
     end
   end
 end
